1ee0ace74506137357387c4b44e9013f0f4e9aa8
Derik Flanary <derikflanary@gmail.com>
2015-10-14 13:28:15 -0600

    Initial Commit

---
+++ SketchyRacer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SketchyRacer
+//
+//  Created by Derik Flanary on 10/14/15.
+//  Copyright © 2015 Derik Flanary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SketchyRacer/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  SketchyRacer
+//
+
+//  Copyright (c) 2015 Derik Flanary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
